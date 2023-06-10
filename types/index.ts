90b785b7731e8f3401a4faaa2e16ab72e8e0a71c
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-06-10 08:49:23 +0300

    feat: done SearchManufacturer

--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,18 @@ export interface CustomButtonProps {
   btnType?: 'button' | 'submit',
   handleClick?: MouseEventHandler<HTMLButtonElement>
 }
+
+export interface SearchManufacturerProps {
+  manufacturer: string,
+  setManufacturer: (manufacturer: string) => void
+}
+
+export interface CustomFilterProps {
+  title: string,
+  options: OptionProps[]
+}
+
+export interface OptionProps {
+  title: string,
+  value: string
+}
