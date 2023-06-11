f3e0f57f578e803e7f9f130caa0c5e59d4d379c1
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-06-11 11:34:47 +0300

    fix: fix env and handleScroll

--- services/index.ts
+++ services/index.ts
@@ -4,7 +4,7 @@ import { FilterProps } from '@/types';
 export const axiosInstance = axios.create({
   baseURL: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
   headers: {
-    'X-RapidAPI-Key': 'b1cce14b03msh65abcc7ffd14045p1933a2jsn14ebf62e2fe3',
+    'X-RapidAPI-Key': process.env.API_KEY,
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
   },
 });
